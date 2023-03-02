ed6dfa624d15212a4dc18b0a2525a97c5f8b3ced
Hoddmimes <par.bertilsson@yahoo.com>
2023-03-02 21:01:24 +0100

    add build version handling

---
+++ server/src/main/java/com/hoddmimes/sailtracker/servlets/VersionServlet.java
@@ -0,0 +1,32 @@
+package com.hoddmimes.sailtracker.servlets;
+
+import com.google.gson.JsonObject;
+import com.hoddmimes.sailtracker.authorize.Authenticator;
+import com.hoddmimes.sailtracker.generated.Version;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+@Path("/version")
+public class VersionServlet {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String isAuthorized(@Context HttpServletRequest servletRequest) {
+        JsonObject jRsp = new JsonObject();
+        JsonObject jBody = new JsonObject();
+
+        jBody.addProperty("build", Version.build);
+        jBody.addProperty("major", Version.major );
+        jBody.addProperty( "minor", Version.minor );
+        jBody.addProperty("buildDate", Version.buildDate );
+
+        jRsp.add("Version", jBody );
+        return jRsp.toString();
+    }
+}
